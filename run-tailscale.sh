39d4148b021c6dcde8960ed052eefd049cf8a1f2
ReivsonAicury <reivson.lopes@aicury.com>
2025-08-30 02:43:42 -0300

    Update run-tailscale.sh
    Definição manual da variavel de rede

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,40 +1,17 @@
 #!/usr/bin/env bash
 
-# /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-# PID=$!
+# Inicia o daemon do tailscale em segundo plano
+/render/tailscaled &
 
-# ADVERTISE_ROUTES=${RENDER_PRIVATE_NETWORK_CIDR}
-# echo "DEBUG: O valor de RENDER_PRIVATE_NETWORK_CIDR é: ${RENDER_PRIVATE_NETWORK_CIDR}"
-# until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
-#   sleep 0.1
-# done
-# export ALL_PROXY=socks5://localhost:1055/
-# tailscale_ip=$(/render/tailscale ip)
-# echo "Tailscale is up at IP ${tailscale_ip}"
+# Aguarda 5 segundos para garantir que o daemon iniciou
+sleep 5
 
-# wait ${PID}
+# Inicia o Tailscale usando a nossa variável de ambiente manual
+# O \ no final das linhas é apenas para formatação
+/render/tailscale up \
+    --authkey="${TAILSCALE_AUTHKEY}" \
+    --advertise-routes="${PRIVATE_NETWORK_CIDR}" \
+    --accept-dns=false
 
-#!/usr/bin/env bash
-
-# Laço para aguardar até que a variável de ambiente do Render esteja disponível
-while [ -z "${RENDER_PRIVATE_NETWORK_CIDR}" ]; do
-  echo "Aguardando a variável RENDER_PRIVATE_NETWORK_CIDR ser definida pelo Render..."
-  sleep 2
-done
-
-echo "Variável RENDER_PRIVATE_NETWORK_CIDR encontrada: ${RENDER_PRIVATE_NETWORK_CIDR}"
-
-/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-PID=$!
-
-ADVERTISE_ROUTES="${RENDER_PRIVATE_NETWORK_CIDR}"
-
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="${ADVERTISE_ROUTES}"; do
-    sleep 0.1
-done
-
-export ALL_PROXY=socks5://localhost:1055/
-tailscale ip -4 >/render/tailscale_ip
-echo "Tailscale is up at IP $(tailscale ip)"
-
-wait ${PID}
+# Comando para manter o script e o container rodando indefinidamente
+wait
